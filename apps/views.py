871fb6a5e7d4d18f0a64b0c12a7187ecdb9e0f7e
张帅男 <zhangshuainan@tbkt.cn>
2019-09-18 10:21:30 +0800

    参数验证

--- apps/views.py
+++ apps/views.py
@@ -4,6 +4,8 @@ from libs.utils import ajax
 def login(request):
     username = request.QUERY.get("username")
     password = request.QUERY.get("password")
+    if not username or not password:
+        return ajax.ajax_fail(message="请输入账号密码")
     rel_pwd = rd.get(username)
     if rel_pwd == password:
         return ajax.ajax_ok(message="登陆成功")
